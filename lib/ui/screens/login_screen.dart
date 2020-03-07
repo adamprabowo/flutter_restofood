7843a0a28e0f471d3a8cf30f3dd9b70ab5246de0
adampra28 <adamprabowo00@gmail.com>
2020-03-07 20:05:41 +0700

    Update Auth Login
    update auth login dan view profile

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:restofood_api/core/services/auth_services.dart';
 import 'package:restofood_api/core/utils/toast_utils.dart';
 import 'package:restofood_api/ui/screens/home_screen.dart';
 import 'package:restofood_api/ui/widgets/input_field.dart';
 import 'package:restofood_api/ui/widgets/primary_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -13,60 +15,100 @@ class LoginScreen extends StatelessWidget {
         statusBarColor: Colors.transparent,
       ),
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: LoginBody()
-        ),
+        body: SingleChildScrollView(child: LoginBody()),
       ),
     );
   }
 }
 
-class LoginBody extends StatelessWidget {
+class LoginBody extends StatefulWidget {
+  @override
+  _LoginBodyState createState() => _LoginBodyState();
+}
 
+class _LoginBodyState extends State<LoginBody> {
   var usernameController = TextEditingController();
   var passwordController = TextEditingController();
 
-  void login(BuildContext context) {
-    if (usernameController.text.isNotEmpty && passwordController.text.isNotEmpty) {
-      Navigator.pushNamedAndRemoveUntil(context, "/dashboard", (Route<dynamic> routes) => false);
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> login(BuildContext context) async {
+    if (usernameController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty) {
+      Map<String, dynamic> data = {
+        "username": usernameController.text,
+        "password": passwordController.text
+      };
+
+      ToastUtils.show("Mencoba Login");
+      var response = await AuthServices.login2(data);
+
+      if (response.status == 200) {
+        print("Status:" + response.status.toString());
+        print("message:" + response.message.toString());
+        savePref(usernameController.text, response.data.email.toString(),
+            response.data.pathPhoto.toString(), response.data.kota.toString());
+        Navigator.pushNamedAndRemoveUntil(
+            context, "/dashboard", (Route<dynamic> routes) => false);
+      }
     } else {
       ToastUtils.show("Silahkan isi semua field");
     }
   }
 
+  void savePref(String username, String email, String foto, String kota) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      pref.setString('username', username);
+      pref.setString('email', email);
+      pref.setString('foto', foto);
+      pref.setString('kota', kota);
+    });
+  }
+
+  getPref() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    final username = pref.getString('username');
+
+    if (username != null) {
+      Navigator.pushNamedAndRemoveUntil(
+          context, "/dashboard", (Route<dynamic> routes) => false);
+    }
+  }
+
   void register(BuildContext context) {
-      Navigator.pushNamed(context, "/register");
+    Navigator.pushNamed(context, "/register");
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-
         //Bagian headers
         Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 2.5,
-          color: Colors.orange,
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(Icons.fastfood, size: 40, color: Colors.white),
-                SizedBox(height: 10),
-                Text(
-                  "Restofood",
-                  style: TextStyle(
-                    fontSize: 35, 
-                    color: Colors.white, 
-                    fontWeight: FontWeight.bold
-                  ),
-                )
-              ],
-            ),
-          )
-        ),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2.5,
+            color: Colors.orange,
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.fastfood, size: 40, color: Colors.white),
+                  SizedBox(height: 10),
+                  Text(
+                    "Restofood",
+                    style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            )),
 
         //Bagian field login
         Padding(
@@ -97,7 +139,6 @@ class LoginBody extends StatelessWidget {
                   onClick: () => login(context),
                 ),
               ),
-
               SizedBox(height: 15),
               Container(
                 width: MediaQuery.of(context).size.width,
@@ -111,8 +152,6 @@ class LoginBody extends StatelessWidget {
             ],
           ),
         )
-
-
       ],
     );
   }
--- lib/ui/screens/profile_screen.dart
+++ lib/ui/screens/profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:restofood_api/core/utils/toast_utils.dart';
+import 'package:restofood_api/ui/widgets/primary_button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -9,66 +12,110 @@ class ProfileScreen extends StatelessWidget {
         statusBarColor: Colors.transparent,
       ),
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: ProfileBody()
-        ),
+        body: SingleChildScrollView(child: ProfileBody()),
       ),
     );
   }
 }
 
-class ProfileBody extends StatelessWidget {
+class ProfileBody extends StatefulWidget {
+  @override
+  _ProfileBodyState createState() => _ProfileBodyState();
+}
+
+class _ProfileBodyState extends State<ProfileBody> {
+  String username, email, pathPhoto, kota;
+
+  @override
+  void initState(){
+    super.initState();
+    getPref();
+  }
+
+  logout(){
+    ToastUtils.show("Mencoba Logout");
+    savePref();
+    Future.delayed(const Duration(milliseconds: 2000), (){
+      ToastUtils.show("Berhasil Logout");
+      Navigator.pushNamedAndRemoveUntil(context, "/login", (Route<dynamic> routes) => false);
+    });
+  }
+
+  getPref() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+
+    setState(() {
+      username = pref.getString("username");
+      email = pref.getString("email");
+      pathPhoto = pref.getString("foto");
+      kota = pref.getString("kota");
+    });
+
+    if (username != null) {
+    } else {
+      Navigator.pushNamedAndRemoveUntil(
+          context, "/login", (Route<dynamic> routes) => false);
+    }
+  }
+
+  savePref() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      pref.remove("username");
+      pref.remove("email");
+      pref.remove("profile");
+      pref.remove("kota");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-
         //Bagian headers
         Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 2.5,
-          color: Colors.orange,
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(60),
-                    image: DecorationImage(
-                      image: NetworkImage("https://source.unsplash.com/800x800/?user")
-                    )
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2.5,
+            color: Colors.orange,
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(60),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                pathPhoto.toString()))),
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Yusril Rapsanjani",
-                  style: TextStyle(
-                    fontSize: 25, 
-                    color: Colors.white, 
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                SizedBox(height: 5),
-                Container(
-                  width: MediaQuery.of(context).size.width / 1.8,
-                  child: Text(
-                    "Seorang pria dengan dedikasi tinggi dan haus akan ilmu baru",
-                    textAlign: TextAlign.center,
+                  SizedBox(height: 10),
+                  Text(
+                    "${username.toString()}",
+                    // "Adam Prabowo",
                     style: TextStyle(
-                      fontSize: 15, 
-                      color: Colors.white, 
-                    ),
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
                   ),
-                )
-              ],
-            ),
-          )
-        ),
+                  SizedBox(height: 5),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.8,
+                    child: Text(
+                      "Seorang pria dengan dedikasi tinggi dan haus akan ilmu baru",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )),
 
         //Bagian field login
         Padding(
@@ -87,18 +134,13 @@ class ProfileBody extends StatelessWidget {
                         Text(
                           "Biodata",
                           style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold
-                          ),
+                              fontSize: 15, fontWeight: FontWeight.bold),
                         ),
                         Text(
-                          "Asal Kota: Jakarta\nUmur: 21\nHobby: Programming, Watching Movie",
+                          "Asal Kota: ${kota.toString()}",
                           maxLines: 3,
                           overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.black87
-                          ),
+                          style: TextStyle(fontSize: 14, color: Colors.black87),
                         ),
                       ],
                     ),
@@ -107,9 +149,21 @@ class ProfileBody extends StatelessWidget {
               )
             ],
           ),
+        ),
+        //Button Logout
+        SizedBox(height: 15),
+        Container(
+          margin: EdgeInsets.all(10),
+          width: MediaQuery.of(context).size.width,
+          height: 45,
+          child: PrimaryButton(
+            color: Colors.orange,
+            text: "LOGOUT",
+            onClick: (){
+              logout();
+            }
+          )
         )
-
-
       ],
     );
   }
